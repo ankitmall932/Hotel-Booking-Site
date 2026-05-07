3a68531c9c4fc9bbb48f7057a3c6be37afa93066
Ankit Mall <ankitmall932@gmail.com>
2026-05-07 11:08:22 +0530

    improve global error

--- backend/src/middleware/global.error.js
+++ backend/src/middleware/global.error.js
@@ -1,13 +1,36 @@
-
 const validationError = (err) => {
     const errors = Object.values(err.errors).map(el => el.message);
+
     return {
         statusCode: 400,
         message: errors.join(', ')
     };
 };
 
-export const globalErrorHandler = ((err, req, res, next) => {
+const castError = (err) => {
+    return {
+        statusCode: 400,
+        message: `Invalid ${ err.path }: ${ err.value }`
+    };
+};
+
+const duplicateKeyError = (err) => {
+    const field = Object.keys(err.keyValue)[ 0 ];
+
+    return {
+        statusCode: 400,
+        message: `${ field } already exists`
+    };
+};
+
+const jwtError = () => {
+    return {
+        statusCode: 401,
+        message: 'Invalid Token Please Login Again'
+    };
+};
+
+export const globalErrorHandler = (err, req, res, next) => {
     console.log(err);
     let error = {
         statusCode: err.statusCode || 500,
@@ -17,8 +40,20 @@ export const globalErrorHandler = ((err, req, res, next) => {
     {
         error = validationError(err);
     }
+    if (err.name === 'CastError')
+    {
+        error = castError(err);
+    }
+    if (err.code === 11000)
+    {
+        error = duplicateKeyError(err);
+    }
+    if (err.name === 'JsonWebTokenError')
+    {
+        error = jwtError();
+    }
     res.status(error.statusCode).json({
         success: false,
         message: error.message
     });
-});
+};
