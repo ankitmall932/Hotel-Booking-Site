c4240ad693d279443861211e632cde9b5bd6cee0
Ankit Mall <ankitmall932@gmail.com>
2026-05-07 17:49:16 +0530

    added helmet

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
@@ -11,6 +12,7 @@ import reviewRouter from './routes/review.route.js';
 
 const app = express();
 app.use(cookieParser());
+app.use(helmet());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
