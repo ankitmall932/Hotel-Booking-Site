091d379f7548ecf48b67e553bcc25ffb3d1c23e9
Ankit Mall <ankitmall932@gmail.com>
2026-05-13 14:49:21 +0530

    user added to zustand to make ui best

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react';
 import Router from './routes/Router';
 import { ToastContainer } from 'react-toastify';
 import { useWishlistStore } from './store/wishlistStore';
+import { useAuth } from '../src/context/AuthContext';
 function App () {
   const fetchWishlist = useWishlistStore((state) => state.fetchWishlist);
   useEffect(() => {
     fetchWishlist();
-  }, [ fetchWishlist ]);
+  }, [ useAuth(), fetchWishlist ]);
   return (
     <div>
       <ToastContainer
