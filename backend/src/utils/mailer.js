e325926a0b4d4511b617ceaafdbba4b8aa759405
Ankit Mall <ankitmall932@gmail.com>
2026-05-15 21:26:20 +0530

    bug2

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -5,7 +5,8 @@ console.log({
     host: process.env.BREVO_SMTP_HOST,
     port: Number(process.env.BREVO_SMTP_PORT),
     user: process.env.BREVO_SMTP_USERNAME,
-    pass: process.env.BREVO_SMTP_PASSWORD
+    pass: process.env.BREVO_SMTP_PASSWORD,
+    email: process.env.MAIL_FROM
 });
 export const transporter = nodemailer.createTransport({
     host: process.env.BREVO_SMTP_HOST,
@@ -14,5 +15,11 @@ export const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.BREVO_SMTP_USERNAME,
         pass: process.env.BREVO_SMTP_PASSWORD
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
+
+await transporter.verify();
+console.log("SMTP connected successfully");
