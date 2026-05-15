f766560adbdae0e932175e9ac60e9bffd1240111
Ankit Mall <ankitmall932@gmail.com>
2026-05-15 21:50:24 +0530

    email bug cleared

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,13 +1,6 @@
 import 'dotenv/config';
 import nodemailer from 'nodemailer';
 
-console.log({
-    host: process.env.BREVO_SMTP_HOST,
-    port: Number(process.env.BREVO_SMTP_PORT),
-    user: process.env.BREVO_SMTP_USERNAME,
-    pass: process.env.BREVO_SMTP_PASSWORD,
-    email: process.env.MAIL_FROM
-});
 export const transporter = nodemailer.createTransport({
     host: process.env.BREVO_SMTP_HOST,
     port: Number(process.env.BREVO_SMTP_PORT),
@@ -20,14 +13,3 @@ export const transporter = nodemailer.createTransport({
     greetingTimeout: 10000,
     socketTimeout: 10000
 });
-
-(async () => {
-    try
-    {
-        await transporter.verify();
-        console.log("SMTP connected");
-    } catch (err)
-    {
-        console.error(err);
-    }
-})();
