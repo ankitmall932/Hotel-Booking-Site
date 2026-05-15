227ea94c651e1790ab2e8a0b97b05c02cc8c4993
Ankit Mall <ankitmall932@gmail.com>
2026-05-15 21:21:07 +0530

    deploy bug 01

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,6 +1,12 @@
 import 'dotenv/config';
 import nodemailer from 'nodemailer';
 
+console.log({
+    host: process.env.BREVO_SMTP_HOST,
+    port: Number(process.env.BREVO_SMTP_PORT),
+    user: process.env.BREVO_SMTP_USERNAME,
+    pass: process.env.BREVO_SMTP_PASSWORD
+});
 export const transporter = nodemailer.createTransport({
     host: process.env.BREVO_SMTP_HOST,
     port: Number(process.env.BREVO_SMTP_PORT),
